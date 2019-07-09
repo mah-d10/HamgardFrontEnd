6a52d6cc790a0f9f6ab670f5349ed6af20b58bb0
Mahdi Moqaddami <mah_d10@outlook.com>
2019-07-09 18:02:53 +0430

    Add functionality to 'groups-new' component.

--- src/app/groups/groups-new/groups-new.component.ts
+++ src/app/groups/groups-new/groups-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import {GroupsService} from '../groups.service';
 
 @Component({
   selector: 'app-groups-new',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private grpService: GroupsService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    const name = form.value.name;
+    const u1Email = form.value.u1;
+    const u2Email = form.value.u2;
+    const u3Email = form.value.u3;
+    this.grpService.createGroup(name, u1Email, u2Email, u3Email)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        }
+      );
   }
 
 }
