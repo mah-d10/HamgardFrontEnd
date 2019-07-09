25889b1d89536645fcc21daf0459c3e7b89e4a22
Mahdi Moqaddami <mah_d10@outlook.com>
2019-07-10 01:39:31 +0430

    Add the required methods to 'events' service.

---
+++ src/app/events/events.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthService} from '../auth/auth.service';
+
+@Injectable()
+export class EventsService {
+
+  constructor(private http: HttpClient,
+              private authService: AuthService) {
+  }
+
+  host = this.authService.host;
+
+  events = [];
+
+  getEvents() {
+    const hs = new HttpHeaders({Authorization: this.authService.getToken()});
+    return this.http.get(this.host + '/event/api/v1/events/', {headers: hs});
+  }
+
+  search(searchString: string) {
+    const hs = new HttpHeaders({Authorization: this.authService.getToken()});
+    return this.http.get(this.host + '/event/api/v1/places/search', {
+      params: {q: searchString}, headers: hs
+    });
+  }
+
+  getEventIDbyIndex(index: number) {
+    return 5;
+    // to do
+  }
+}
